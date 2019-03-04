1e17458c94afbf35cf367b60ec3c4c19817354e0
Whisperering <emielko@stu.naperville203.org>
2019-03-04 07:39:27 -0600

    Worked on triangle GUI step 5

--- Triangle GUI/TriangleComponent.java
+++ Triangle GUI/TriangleComponent.java
@@ -11,8 +11,12 @@ public class TriangleComponent extends JComponent
 {
     private static final int MAX_POINTS = 3;
     private int clicks;
-    private int x;
-    private int y;
+    private int p1x;
+    private int p1y;
+    private int p2x;
+    private int p2y;
+    private int p3x;
+    private int p3y;
     public TriangleComponent()
     {
         MouseListener listener = new MouseClickListener();
@@ -27,14 +31,19 @@ public class TriangleComponent extends JComponent
         Graphics2D g2 = (Graphics2D) g;
         if(clicks == 1)
         {
-            Ellipse2D.Double point = new Ellipse2D.Double(x - 15,
-                    y - 15, 30, 30);
+            Ellipse2D.Double point = new Ellipse2D.Double(p1x - 5,
+                    p1y - 5, 10, 10);
             g2.draw(point);
         }
         else if(clicks == 2)
         {
-            Ellipse2D.Double point2 = new Ellipse2D.Double(x - 15,
-                    y - 15, 30, 30);
+            Ellipse2D.Double point1 = new Ellipse2D.Double(p1x - 5,
+                    p1y - 5, 10, 10);
+            Ellipse2D.Double point2 = new Ellipse2D.Double(p2x - 5,
+                    p2y - 5, 10, 10);
+            Line2D.Double line = new Line2D.Double(p1x, p1y, p2x, p2y);
+            g2.draw(point1);
+            g2.draw(line);
             g2.draw(point2);
         }
         else if(clicks == 3)
@@ -44,6 +53,26 @@ public class TriangleComponent extends JComponent
         {
         }
     }
+    
+    public void addPoint(int x, int y)
+    {
+        if(clicks == 1)
+        {
+            p1x = x;
+            p1x = y;
+        }
+        else if(clicks == 2)
+        {
+            p2x = x;
+            p2x = y;
+        }
+        else if(clicks == 3)
+        {
+            p3x = x;
+            p3x = y;
+        }
+    }
+    
     public class MouseClickListener implements MouseListener
     {
         public void mouseClicked( MouseEvent event )
@@ -58,6 +87,8 @@ public class TriangleComponent extends JComponent
             {
                 clicks = 0;
             }
+            addPoint(event.getX(), event.getY());
+            repaint();
         }
 
         public void mouseEntered( MouseEvent event ){}
