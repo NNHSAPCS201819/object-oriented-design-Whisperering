69255bf945ffb19ae9d54dcd8eeab9072a252a05
Whisperering <emielko@stu.naperville203.org>
2019-03-15 12:00:28 -0500

    Worked on implementing teh DrawingPanel

--- Drawing Editor Lab/DrawingPanel.java
+++ Drawing Editor Lab/DrawingPanel.java
@@ -3,6 +3,10 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.awt.Dimension;
+import java.awt.Graphics;
 /**
  * The panel in which draws all of the shapes in the drawing editor
  * 
@@ -12,13 +16,17 @@ import java.awt.event.MouseMotionListener;
 public class DrawingPanel extends JPanel
 {
     Color fillColor;
-    
+    private ArrayList<DrawingShape> shapes;
 
     /**
      * Default constructor for objects of class DrawingPanel
      */
     public DrawingPanel()
     {
+        MouseListener listener = new MouseClickListener();
+        MouseMotionListener mlistener = new MouseMovedListener();
+        this.addMouseListener(listener);
+        this.addMouseMotionListener(mlistener);
     }
     
     /**
@@ -40,5 +48,53 @@ public class DrawingPanel extends JPanel
     public Color getColor()
     {
         return fillColor;
+    }
+    
+    public Dimension getPreferredSize()
+    {
+        Dimension newSize = new Dimension(500, 500);
+        return newSize;
+    }
+    
+    public void addCircle()
+    {
+    }
+    
+    public void Square()
+    {
+    }
+    
+    public void paintComponent(Graphics g)
+    {
+    }
+    
+    public class MouseClickListener implements MouseListener
+    {
+        public void mouseClicked(MouseEvent event)
+        {
+        }
+        public void mouseEntered(MouseEvent event)
+        {
+        }
+        public void mouseExited(MouseEvent event)
+        {
+        }
+        public void mousePressed(MouseEvent event)
+        {
+            //
+        }
+        public void mouseReleased(MouseEvent event)
+        {
+        }
+    }
+    public class MouseMovedListener implements MouseMotionListener
+    {
+        public void mouseMoved(MouseEvent event)
+        {
+        }
+        public void mouseDragged(MouseEvent event)
+        {
+            //
+        }
     }
 }
