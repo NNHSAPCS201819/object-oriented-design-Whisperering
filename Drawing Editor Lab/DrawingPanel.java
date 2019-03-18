48ce59fb22099e6f5479c25ecdb9a67023b05d83
Whisperering <emielko@stu.naperville203.org>
2019-03-18 07:41:53 -0500

    worked on say 4 finished paintComponent

--- Drawing Editor Lab/DrawingPanel.java
+++ Drawing Editor Lab/DrawingPanel.java
@@ -7,7 +7,9 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.geom.Point2D.Double;
+import java.awt.geom.Point2D;
 /**
  * The panel in which draws all of the shapes in the drawing editor
  * 
@@ -20,6 +22,7 @@ public class DrawingPanel extends JPanel
     private ArrayList<DrawingShape> shapes;
     private Point2D.Double point;
     private Dimension size;
+    private DrawingShape selected;
     /**
      * Default constructor for objects of class DrawingPanel
      */
@@ -30,9 +33,9 @@ public class DrawingPanel extends JPanel
         this.addMouseListener(listener);
         this.addMouseMotionListener(mlistener);
         shapes = new ArrayList<DrawingShape>();
-        point.setLocation((size.getHeight()/2),(size.getWidth()/2));
+        point = new Point2D.Double((size.getHeight()/2),(size.getWidth()/2));
     }
-    
+
     /**
      * Invoked when the "Pick Color" button is clicked. Displays a JColorChooser and sets the
      *  selected color as the new fill color. Leaves the fill color unchanged if the user clicks
@@ -42,7 +45,7 @@ public class DrawingPanel extends JPanel
     public void pickColor()
     {
         Color selectedColor = JColorChooser.showDialog( this, "select the fill color", this.fillColor );
-        
+
         if( selectedColor != null )
         {
             this.fillColor = selectedColor;
@@ -53,51 +56,59 @@ public class DrawingPanel extends JPanel
     {
         return fillColor;
     }
-    
+
     public Dimension getPreferredSize()
     {
         Dimension newSize = new Dimension(500, 500);
         newSize = size;
         return size;
     }
-    
+
     public void addCircle()
     {
         shapes.add(new Circle(point, Math.random() * 24 + 1,fillColor));
+        selected = shapes.get(shapes.size() - 1);
     }
-    
+
     public void addSquare()
     {
         shapes.add(new Square(point, Math.random() * 24 + 1,fillColor));
+        selected = shapes.get(shapes.size() - 1);
     }
-    
+
     public void paintComponent(Graphics g)
     {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
         for(DrawingShape shape : shapes)
         {
-            shape.draw( , true);
-            if(shapes.get(shape) == )
+            shape.draw(g2 , true);
+            if(shape == selected)
             {
-                shape.draw( , false);
+                shape.draw(g2 , false);
             }
         }
     }
-    
+
     public class MouseClickListener implements MouseListener
     {
         public void mouseClicked(MouseEvent event)
         {
         }
+
         public void mouseEntered(MouseEvent event)
         {
         }
+
         public void mouseExited(MouseEvent event)
         {
         }
+
         public void mousePressed(MouseEvent event)
         {
             //
         }
+
         public void mouseReleased(MouseEvent event)
         {
         }
@@ -107,6 +118,7 @@ public class DrawingPanel extends JPanel
         public void mouseMoved(MouseEvent event)
         {
         }
+
         public void mouseDragged(MouseEvent event)
         {
             //
